ca4d0b2a87b686e87e78c3b555de89a0747fd81b
James Lee <jclee906@gmail.com>
2026-04-06 10:08:06 +0100

    Remove duplicate nested directory and stale files

--- matcher/core/PriceLevel.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Order.h"
-
-struct PriceLevel {
-    Order* head = nullptr;
-    Order* tail = nullptr;
-};
--- matcher/core/TradeLog.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cstdint>
-
-struct TradeLog {
-    uint64_t makerOrderId;
-    uint64_t takerOrderId;
-    int64_t price;
-    int64_t quantity;
-};
